fec6beca9640da6aa063dbe220f22427922ab027
fismapelajar <90302378+fismapelajar@users.noreply.github.com>
2021-10-29 10:03:39 +0700

    Add files via upload

---
+++ FismaMeividianugrahaSubani_017_MatriksPengurangan.cpp
@@ -0,0 +1,39 @@
+//Tugas 5
+//Program Pengurangan Matriks
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int i, j, m, n, matriks1[10][10], matriks2[10][10], hasil[10][10];
+	
+	cout<<"\t Pengurangan Matriks "<<endl;
+	cout<<"Masukkan jumlah baris matriks = ";
+	cin>>m;
+	cout<<"Masukkan jumlah kolom matriks = ";
+	cin>>n;
+	
+	cout<<"Masukkan elemen matriks pertama = \n";
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+			cin>>matriks1[i][j];
+		}
+	}
+	
+	cout<<"Masukkan elemen matriks ke dua = \n";
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+			cin>>matriks2[i][j];
+		}
+	}
+	
+	cout<<"Hasil pengurangan mantrik = \n";
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+			hasil[i][j] = matriks1[i][j]-matriks2[i][j];
+			cout<<hasil[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
